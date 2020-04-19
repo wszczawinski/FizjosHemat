85578bed9891f8d20c77259ee5cd57d3f1a8fb0b
wszczawinski <w.k.szczawinski@gmail.com>
2020-04-19 12:33:13 +0200

    docs(descriptions): steing manifest, readme, config

--- src/components/Head.js
+++ src/components/Head.js
@@ -4,10 +4,10 @@ import { Helmet } from 'react-helmet';
 const Head = () => {
     return (
         <Helmet>
-          <meta charSet="utf-8" />
-          <title>FizjosHemat</title>
-          <meta name="description" content="Blog na temat fizjoterapi w hematologi." />
-          <meta name="keywords" content="fizjoteratia, hematologia" />
+            <meta charSet="utf-8" />
+            <title>FizjosHemat</title>
+            <meta name="description" content="Blog na temat fizjoterapi w hematologi." />
+            <meta name="keywords" content="fizjoteratia, hematologia" />
         </Helmet>
     );
 };
