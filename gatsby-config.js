decb02af511d0f885e1cdd566bf4e7da40018498
wszczawinski <w.k.szczawinski@gmail.com>
2020-04-20 17:12:43 +0200

    feat(postImage): finish blog based on markdown and enable images in posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,6 @@
 module.exports = {
     plugins: [
         `gatsby-plugin-sass`,
-        `gatsby-plugin-react-helmet`,
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: {
-                name: 'src',
-                path: `${__dirname}/src/`
-            }
-        },
-        'gatsby-transformer-remark',
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
@@ -17,9 +8,33 @@ module.exports = {
                 short_name: `FizjosHemat`,
                 start_url: `/`,
                 icon: `src/images/FizjosHematFavicon.png`,
-                display: "standalone",
+                display: 'standalone',
                 crossOrigin: `use-credentials`,
                 legacy: false,
+            },
+        },
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'src',
+                path: `${__dirname}/src/`,
+            },
+        },
+        `gatsby-plugin-sharp`,
+        {
+            resolve: 'gatsby-transformer-remark',
+            options: {
+                plugins: [
+                    'gatsby-remark-relative-images',
+                    {
+                        resolve: 'gatsby-remark-images',
+                        options: {
+                            maxWidth: 800,
+                            linkImagesToOriginal: false,
+                        },
+                    },
+                ],
             },
         },
     ],
--- src/posts/First/first.md
+++ src/posts/First/first.md
@@ -5,6 +5,8 @@ date: '2020-04-04'
 
 I just started this blog!
 
+![deer](./running.png)
+
 ## Banana
 
 1. Nananan
