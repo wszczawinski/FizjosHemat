19a79662c8d7b2b3216437f5834df9f28dda2335
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-05-10 22:54:19 +0200

    feat(index): styling

--- src/components/IndexSections/AboutSection.js
+++ src/components/IndexSections/AboutSection.js
@@ -7,28 +7,34 @@ export const AboutSection = () => {
         <>
             <article className={styles.aboutArticle}>
                 <img src={MaciekImage} alt="" className={styles.aboutImage} />
-                <h3 className={styles.aboutTitle}>Mgr Maciek</h3>
-                <p className={styles.aboutDescription}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
-                    tenetur aperiam sequi, consequuntur, tempora sed, nisi voluptatibus
-                    eos omnis enim adipisci? Error fuga perspiciatis incidunt dolorem quas
-                    debitis blanditiis repellendus adipisci nobis dolorum nesciunt vero,
-                    excepturi sapiente ratione cupiditate accusamus?
-                </p>
+                <div className={styles.aboutDescription}>
+                    <h3 className={styles.aboutTitle}>Mgr Maciek</h3>
+                    <p className={styles.aboutText}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Architecto tenetur aperiam sequi, consequuntur, tempora sed, nisi
+                        voluptatibus eos omnis enim adipisci? Error fuga perspiciatis
+                        incidunt dolorem quas debitis blanditiis repellendus adipisci
+                        nobis dolorum nesciunt vero, excepturi sapiente ratione cupiditate
+                        accusamus?
+                    </p>
+                </div>
             </article>
             <button className={styles.btn} href="/about/">
                 O nas
             </button>
             <article className={styles.aboutArticle}>
+                <div className={styles.aboutDescription}>
+                    <h3 className={styles.aboutTitle}>Mgr Maciek</h3>
+                    <p className={styles.aboutText}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Architecto tenetur aperiam sequi, consequuntur, tempora sed, nisi
+                        voluptatibus eos omnis enim adipisci? Error fuga perspiciatis
+                        incidunt dolorem quas debitis blanditiis repellendus adipisci
+                        nobis dolorum nesciunt vero, excepturi sapiente ratione cupiditate
+                        accusamus?
+                    </p>
+                </div>
                 <img src={MaciekImage} alt="" className={styles.aboutImage} />
-                <h3 className={styles.aboutTitle}>Mgr Maciek</h3>
-                <p className={styles.aboutDescription}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
-                    tenetur aperiam sequi, consequuntur, tempora sed, nisi voluptatibus
-                    eos omnis enim adipisci? Error fuga perspiciatis incidunt dolorem quas
-                    debitis blanditiis repellendus adipisci nobis dolorum nesciunt vero,
-                    excepturi sapiente ratione cupiditate accusamus?
-                </p>
             </article>
         </>
     );
