3c109c66a0873deb67254a85a577e94246f44e8a
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-05-16 20:09:15 +0200

    feat(indexContent): fonts, about real descriptions, images naming + delete

--- src/components/IndexSections/AboutSection.js
+++ src/components/IndexSections/AboutSection.js
@@ -9,14 +9,15 @@ export const AboutSection = () => {
             <article className={styles.aboutArticle}>
                 <img src={OlaImage} alt="" className={styles.aboutImage} />
                 <div className={styles.aboutDescription}>
-                    <h3 className={styles.aboutTitle}>Mgr Ola</h3>
-                    <p className={styles.aboutText}>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                        Architecto tenetur aperiam sequi, consequuntur, tempora sed, nisi
-                        voluptatibus eos omnis enim adipisci? Error fuga perspiciatis
-                        incidunt dolorem quas debitis blanditiis repellendus adipisci
-                        nobis dolorum nesciunt vero, excepturi sapiente ratione cupiditate
-                        accusamus?
+                    <h3 className={styles.aboutTitle}>mgr Aleksandra Bulińska</h3>
+                    <p className={`${styles.aboutText}`}>
+                        Kierują się holistycznym spojrzeniem, dbam nie tylko o naprawienie
+                        problemu fizycznego, ale również zwracam baczną uwagę na
+                        poprawienie ogólnej jakości życia pacjenta. Największym wyzwaniem
+                        jest dla mnie praca z pacjentami przygotowującymi się do
+                        transplantacji krwiotwórczych komórek macierzystych szpiku oraz w
+                        trakcie tej procedury. Poza hematologią duży obszar moich
+                        zawodowych działań wiąże się z fizjoterapią oddechową.
                     </p>
                 </div>
             </article>
@@ -25,14 +26,15 @@ export const AboutSection = () => {
             </button>
             <article className={styles.aboutArticle}>
                 <div className={styles.aboutDescription}>
-                    <h3 className={styles.aboutTitle}>Mgr Maciek</h3>
+                    <h3 className={styles.aboutTitle}>mgr Maciej Koktysz</h3>
                     <p className={styles.aboutText}>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                        Architecto tenetur aperiam sequi, consequuntur, tempora sed, nisi
-                        voluptatibus eos omnis enim adipisci? Error fuga perspiciatis
-                        incidunt dolorem quas debitis blanditiis repellendus adipisci
-                        nobis dolorum nesciunt vero, excepturi sapiente ratione cupiditate
-                        accusamus?
+                        Jestem pasjonatem ruchu i możliwości ludzkiego ciała. Do każdego
+                        pacjenta podchodzę indywidualnie, bazując na jego aktualnych
+                        możliwościach. Najważniejsze dla mnie w mojej pracy jest pozytywne
+                        nastawienie, wiara w osiągnięciu wspólnego celu i dbałość o jak
+                        najlepszą jakość życia moich pacjentów. Poza onkologią,
+                        specjalizuje się w fizjoterapii po urazach i operacjach
+                        ortopedycznych oraz chorobach i zespołach bólowych kręgosłupa.
                     </p>
                 </div>
                 <img src={MaciekImage} alt="" className={styles.aboutImage} />
