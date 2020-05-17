fb22c6c273a98e21b780c6428307575b1808afac
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-05-17 23:42:17 +0200

    feat(indexContent): arrows down

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,13 @@ import { AboutSection, BlogSection, MethodsSection, Layout } from '../components
 const IndexPage = () => {
     return (
         <Layout>
-            <div className={styles.parallax}></div>
+            <div className={styles.parallax}>
+                <div className={styles.arrows}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
             <main className={styles.home}>
                 <section className={styles.about}>
                     <AboutSection />
