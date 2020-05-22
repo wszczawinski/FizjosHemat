1ecfcac033b27f83f0a1ea3fd3396b7162a01416
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-05-22 23:08:36 +0200

    feat(blogScetion): initial styling

--- src/components/IndexSections/BlogSection.js
+++ src/components/IndexSections/BlogSection.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import styles from './index.module.scss';
+import blogImage from '../../images/guy.jpg';
 
 export const BlogSection = () => {
     return (
         <>
-            <article>Ola about</article>
+            <article className={styles.blogArticle}>
+                <img className={styles.blogImage} src={blogImage} alt="" />
+                <h3 className={styles.blogTitle}>Blog</h3>
+                <p className={styles.blogText}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum vel
+                    voluptate harum a porro! Omnis sequi, id, natus labore deleniti nobis
+                    voluptatibus fugit reprehenderit, porro dolorem consequatur tempore
+                    alias quidem.
+                </p>
+            </article>
             <a className={styles.btn} href="/blog/">
                 Blog
             </a>
-            <article>Maciek about</article>
+            <article className={styles.blogArticle}>
+                <img className={styles.blogImage} src={blogImage} alt="" />
+                <h3 className={styles.blogTitle}>Blog</h3>
+                <p className={styles.blogText}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum vel
+                    voluptate harum a porro! Omnis sequi, id, natus labore deleniti nobis
+                    voluptatibus fugit reprehenderit, porro dolorem consequatur tempore
+                    alias quidem.
+                </p>
+            </article>
         </>
     );
 };
