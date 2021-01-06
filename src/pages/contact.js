fc4cac09717d412045eaf8c2404a536eca31d5d4
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2021-01-06 20:32:53 +0100

    feat(contactStyling): initial styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import styles from './contact.module.scss';
 import { Layout } from '../components';
 
 const ContactPage = () => {
     return (
         <Layout>
-            
+            <main className={styles.container}>
+                <h2 className={styles.contactTitle}>Nasze lokalizacje</h2>
+                <p>W celu umówienia się na konsultacje wystarczy skontaktować się z nami telefonicznie bądź mailowo.</p>
+                <div className={styles.map}>
+                    <iframe
+                        title="Lokalizacje"
+                        src="https://www.google.com/maps/d/embed?mid=1g_Rsutsuv-00PTb3LUJEjuDCw8mE2Q75"
+                        width="100%"
+                        height="100%"
+                    ></iframe>
+                </div>
+            </main>
         </Layout>
     );
 };
